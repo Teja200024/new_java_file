ad94c1392b51a783e3a0c9e85f0b3913b0b0488e
root <root@ip-172-31-46-19.ec2.internal>
2024-03-15 13:43:10 +0000

    first commit

---
+++ myjavafile/src/main/java/com/siva/App.java
@@ -0,0 +1,18 @@
+package com.siva;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+	int a,b,c;
+	a=89;
+	b=102;
+	c=a+b;
+	System.out.println("the sum is"+c);
+        System.out.println( "Hello World!" );
+    }
+}
